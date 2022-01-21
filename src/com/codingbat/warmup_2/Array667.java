af9d497efdd27f87132f3f87c3a267ea0634b7fa
Adam Wandoch <wandoch.adam@gmail.com>
2022-01-21 07:26:40 +0000

    array667

---
+++ src/com/codingbat/warmup_2/Array667.java
@@ -0,0 +1,16 @@
+package com.codingbat.warmup_2;
+
+/**
+ * @author Adam Wandoch
+ */
+public class Array667 {
+    public int array667(int[] nums) {
+        int counter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 < nums.length && nums[i] == 6 && (nums[i + 1] == 6 || nums[i + 1] == 7)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
---
+++ src/com/codingbat/warmup_2/NoTriples.java
@@ -0,0 +1,14 @@
+package com.codingbat.warmup_2;
+
+/**
+ * @author Adam Wandoch
+ */
+public class NoTriples {
+    public static boolean noTriples(int[] nums) {
+        // check for three times in a row
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
