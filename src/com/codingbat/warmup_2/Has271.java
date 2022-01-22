58ebc7b255b30c318af9387fb94b2cfa34553882
Adam Wandoch <wandoch.adam@gmail.com>
2022-01-22 07:30:25 +0000

    noTriples

---
+++ src/com/codingbat/warmup_2/Has271.java
@@ -0,0 +1,15 @@
+package com.codingbat.warmup_2;
+
+/**
+ * @author Adam Wandoch
+ */
+
+//Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value,
+// followed by the value plus 5, followed by the value minus 1. Additionally the 271 counts even
+// if the "1" differs by 2 or less from the correct value.
+
+public class Has271 {
+//    public static boolean has271(int[] nums) {
+//
+//    }
+}
--- src/com/codingbat/warmup_2/NoTriples.java
+++ src/com/codingbat/warmup_2/NoTriples.java
@@ -4,11 +4,17 @@ package com.codingbat.warmup_2;
  * @author Adam Wandoch
  */
 public class NoTriples {
-    public static boolean noTriples(int[] nums) {
-        // check for three times in a row
-    }
-
-    public static void main(String[] args) {
-
+    public boolean noTriples(int[] nums) {
+        if (nums.length > 2) {
+            //int count = 0;
+            for (int i = 0; i < nums.length - 1; i++) {
+                for (int j = i + 1; j < nums.length - 1; j++) {
+                    if (nums[i] == nums[j] && nums[i] == nums[j + 1]) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
