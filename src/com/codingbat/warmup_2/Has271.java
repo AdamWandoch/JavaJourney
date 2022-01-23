60d051ae977e3fc96a64252c74e7316aa903f768
adamwandoch <wandoch.adam@gmail.com>
2022-01-23 00:01:49 +0000

    removed static for consistency

--- src/com/codingbat/warmup_2/Has271.java
+++ src/com/codingbat/warmup_2/Has271.java
@@ -5,7 +5,7 @@ package com.codingbat.warmup_2;
  */
 
 public class Has271 {
-    public static boolean has271(int[] nums) {
+    public boolean has271(int[] nums) {
         if (nums.length > 2) {
             for (int i = 0; i < nums.length - 2; i++) {
                 if (nums[i + 1] == nums[i] + 5 && (nums[i + 2] >= nums[i] - 3) && (nums[i + 2] <= nums[i] + 1)) {
