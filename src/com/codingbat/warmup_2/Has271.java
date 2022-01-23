aa392848186e0235237281bb3c0de25372943d75
adamwandoch <wandoch.adam@gmail.com>
2022-01-23 00:00:54 +0000

    has271

--- src/com/codingbat/warmup_2/Has271.java
+++ src/com/codingbat/warmup_2/Has271.java
@@ -4,12 +4,15 @@ package com.codingbat.warmup_2;
  * @author Adam Wandoch
  */
 
-//Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value,
-// followed by the value plus 5, followed by the value minus 1. Additionally the 271 counts even
-// if the "1" differs by 2 or less from the correct value.
-
 public class Has271 {
-//    public static boolean has271(int[] nums) {
-//
-//    }
+    public static boolean has271(int[] nums) {
+        if (nums.length > 2) {
+            for (int i = 0; i < nums.length - 2; i++) {
+                if (nums[i + 1] == nums[i] + 5 && (nums[i + 2] >= nums[i] - 3) && (nums[i + 2] <= nums[i] + 1)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
