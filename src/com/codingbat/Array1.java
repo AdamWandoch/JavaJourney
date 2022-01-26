57ce6fc5d889de872d1145af3c3e58fb6845ac99
Adam Wandoch <wandoch.adam@gmail.com>
2022-01-26 06:31:46 +0000

    finished Array1.java

--- src/com/codingbat/Array1.java
+++ src/com/codingbat/Array1.java
@@ -127,11 +127,81 @@ public final class Array1 {
         return counter;
     }
 
+    /** https://codingbat.com/prob/p109537 */
+    public static int[] biggerTwo(int[] a, int[] b) {
+        if (a[0] + a[1] < b[0] + b[1]) return b;
+        return a;
+    }
 
+    /** https://codingbat.com/prob/p199519 */
+    public static int[] makeMiddle(int[] nums) {
+        int index = nums.length / 2 - 1;
+        return new int[]{nums[index], nums[index + 1]};
+    }
 
+    /** https://codingbat.com/prob/p180840 */
+    public static int[] plusTwo(int[] a, int[] b) {
+        return new int[]{a[0], a[1], b[0], b[1]};
+    }
 
+     /** https://codingbat.com/prob/p118044 */
+     public static int[] swapEnds(int[] nums) {
+        int temp = nums[0];
+        nums[0] = nums[nums.length - 1];
+        nums[nums.length - 1] = temp;
+        return nums;
+     }
 
+     /** https://codingbat.com/prob/p155713 */
+     public static int[] midThree(int[] nums) {
+        int index = nums.length / 2;
+        return new int[]{nums[index - 1], nums[index], nums[index + 1]};
+     }
 
+     /** https://codingbat.com/prob/p185176 */
+     public static int maxTriple(int[] nums) {
+        int max = nums[0];
+        if (nums[nums.length - 1] > max) max = nums[nums.length - 1];
+        if (nums.length > 2) {
+            if (nums[nums.length / 2] > max) max = nums[nums.length / 2];
+        }
+        return max;
+     }
 
+    /** https://codingbat.com/prob/p142455 */
+    public static int[] frontPiece(int[] nums) {
+        if (nums.length < 2) return nums;
+        return new int[]{nums[0], nums[1]};
+    }
 
+    /** https://codingbat.com/prob/p197308 */
+    public static boolean unlucky1(int[] nums) {
+        if (nums.length < 2) return false;
+        if (nums.length > 2 && nums[1] == 1 && nums[2] == 3) return true;
+        if ((nums[0] == 1 && nums[1] == 3) || (nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3)) return true;
+        return false;
+    }
+    /** https://codingbat.com/prob/p143461 */
+    public static int[] make2(int[] a, int[] b) {
+        int[] result = new int[2];
+        if (a.length > 1) {
+            result[0] = a[0];
+            result[1] = a[1];
+        } else if (a.length == 1) {
+            result[0] = a[0];
+            result[1] = b[0];
+        } else {
+            result[0] = b[0];
+            result[1] = b[1];
+        }
+        return result;
+    }
+
+    /** https://codingbat.com/prob/p128270 */
+    public static int[] front11(int[] a, int[] b) {
+        if (a.length == 0 && b.length == 0) return a;
+        if (a.length == 0) return new int[]{b[0]};
+        if (b.length == 0) return new int[]{a[0]};
+        return new int[]{a[0], b[0]};
+    }
 }
